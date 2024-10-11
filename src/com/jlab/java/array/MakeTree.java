430697cb29d37577eb5a2dec9ab3c793b2632d75
abhishekcse08 <abhishekcse08@gmail.com>
2024-10-11 12:08:54 +0530

    Tree commit

---
+++ src/com/jlab/java/array/MakeTree.java
@@ -0,0 +1,13 @@
+package com.jlab.java.array;
+
+public class MakeTree {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello tree");
+    }
+
+    public void checkTree(){
+
+    }
+}
