2305091b000cdea67c3e61fbde46aba89c72263e
abhishekcse08 <abhishekcse08@gmail.com>
2024-10-11 12:33:35 +0530

    first commit

--- src/com/jlab/java/core/MyTestData.java
+++ src/com/jlab/java/core/MyTestData.java
@@ -4,5 +4,7 @@ public class MyTestData {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello first commit");
+        System.out.println("amend commit");
     }
 }
